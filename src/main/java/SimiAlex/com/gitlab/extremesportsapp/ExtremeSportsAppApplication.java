1273e9281f1f9a23d35af886cb4fb07e89422140
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-03-23 19:35:11 +0200

    Initial project setup

---
+++ src/main/java/SimiAlex/com/gitlab/extremesportsapp/ExtremeSportsAppApplication.java
@@ -0,0 +1,13 @@
+package SimiAlex.com.gitlab.extremesportsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtremeSportsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtremeSportsAppApplication.class, args);
+	}
+
+}
