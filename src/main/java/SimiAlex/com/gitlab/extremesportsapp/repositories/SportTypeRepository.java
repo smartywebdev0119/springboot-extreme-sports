5db6c082d7b250b3b9b577823c86749f9b0f7b4d
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-04-02 19:56:41 +0300

    Implemented query to getSportTypesByName

--- src/main/java/SimiAlex/com/gitlab/extremesportsapp/repositories/SportTypeRepository.java
+++ src/main/java/SimiAlex/com/gitlab/extremesportsapp/repositories/SportTypeRepository.java
@@ -1,9 +1,15 @@
 package SimiAlex.com.gitlab.extremesportsapp.repositories;
 
 import SimiAlex.com.gitlab.extremesportsapp.model.SportType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SportTypeRepository extends CrudRepository<SportType, Long> {
+
+    @Query(value = "SELECT s FROM SportType s WHERE s.sportName IN :sportTypeList")
+    public List<SportType> getSportTypesByName(List<String> sportTypeList);
 }
