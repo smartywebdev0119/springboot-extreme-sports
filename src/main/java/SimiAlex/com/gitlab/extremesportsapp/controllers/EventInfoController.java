5568425dd6417889b557745fbe4abf4a9f1420bd
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-04-02 19:59:58 +0300

    Completed EventInfoController

--- src/main/java/SimiAlex/com/gitlab/extremesportsapp/controllers/EventInfoController.java
+++ src/main/java/SimiAlex/com/gitlab/extremesportsapp/controllers/EventInfoController.java
@@ -5,16 +5,18 @@ import SimiAlex.com.gitlab.extremesportsapp.model.EventInfo;
 import SimiAlex.com.gitlab.extremesportsapp.model.SportType;
 import SimiAlex.com.gitlab.extremesportsapp.model.request.EventInfoRequestModel;
 import SimiAlex.com.gitlab.extremesportsapp.repositories.DestinationRepository;
+import SimiAlex.com.gitlab.extremesportsapp.repositories.EventInfoRepository;
 import SimiAlex.com.gitlab.extremesportsapp.repositories.SportTypeRepository;
 import SimiAlex.com.gitlab.extremesportsapp.services.EventInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("event-info")
@@ -29,6 +31,9 @@ public class EventInfoController {
 
     @Autowired
     private EventInfoService eventInfoService;
+
+    @Autowired
+    private EventInfoRepository eventInfoRepository;
 
     //methods
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -51,4 +56,53 @@ public class EventInfoController {
         return new ResponseEntity<EventInfo>(eventInfoService.createEventInfo(eventInfo), HttpStatus.CREATED);
     }
 
+    @GetMapping(path = "{id}")
+    public ResponseEntity<EventInfo> getEventInfoById(@PathVariable Long id)
+    {
+        return new ResponseEntity<EventInfo>(eventInfoService.getEventInfoById(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventInfo>> getEvents()
+    {
+        return new ResponseEntity<List<EventInfo>>(eventInfoService.getEvents(), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<EventInfo> updateEventInfo(@RequestBody EventInfoRequestModel eventInfoRequestModel, @PathVariable Long id)
+    {
+        //recover event info from database
+        EventInfo eventInfo = eventInfoService.getEventInfoById(id);
+
+        //recover destination and sport type from database
+        Destination destination = destinationRepository.findById(eventInfoRequestModel.getDestinationId()).orElseThrow();
+        SportType sportType = sportTypeRepository.findById(eventInfoRequestModel.getSportTypeId()).orElseThrow();
+
+        //populate event info object
+        eventInfo.setDestination(destination);
+        eventInfo.setSportType(sportType);
+        eventInfo.setPricePerDay(eventInfoRequestModel.getPricePerDay());
+        eventInfo.setBeginDate(eventInfoRequestModel.getBeginDate());
+        eventInfo.setEndDate(eventInfoRequestModel.getEndDate());
+
+        //update event info
+        return new ResponseEntity<EventInfo>(eventInfoService.updateEventInfo(eventInfo), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Void> deleteEventInfo(@PathVariable Long id)
+    {
+        eventInfoService.deleteEventInfo(id);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    @GetMapping(path = "search")
+    public ResponseEntity<List<EventInfo>> getEventsForClientSearch(@RequestParam("sportTypes")List<String> sportTypeParam,
+                                                              @RequestParam("beginDate")@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate beginDateParam,
+                                                              @RequestParam("endDate")@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDateParam)
+    {
+        List<SportType> sportTypeList = sportTypeRepository.getSportTypesByName(sportTypeParam);
+        return new ResponseEntity<List<EventInfo>>(eventInfoRepository.getEventsBySportTypeAndPeriod(sportTypeList, beginDateParam, endDateParam), HttpStatus.OK);
+    }
 }
