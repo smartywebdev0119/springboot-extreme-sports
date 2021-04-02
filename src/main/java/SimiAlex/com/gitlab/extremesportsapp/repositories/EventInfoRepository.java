b4398fb17d7cb652e7543f993ce99f54f023ff2c
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-04-02 19:49:55 +0300

    Implemented main query for client (getEventsBySportTypeAndPeriod)

--- src/main/java/SimiAlex/com/gitlab/extremesportsapp/repositories/EventInfoRepository.java
+++ src/main/java/SimiAlex/com/gitlab/extremesportsapp/repositories/EventInfoRepository.java
@@ -1,9 +1,16 @@
 package SimiAlex.com.gitlab.extremesportsapp.repositories;
 
 import SimiAlex.com.gitlab.extremesportsapp.model.EventInfo;
+import SimiAlex.com.gitlab.extremesportsapp.model.SportType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface EventInfoRepository extends CrudRepository<EventInfo, Long> {
+
+    @Query(value = "SELECT e FROM EventInfo e WHERE e.sportType IN :sportTypesParam AND e.beginDate < :beginDateUser AND e.endDate > :endDateUser")
+    public List<EventInfo> getEventsBySportTypeAndPeriod(List<SportType> sportTypesParam, LocalDate beginDateUser, LocalDate endDateUser);
 }
