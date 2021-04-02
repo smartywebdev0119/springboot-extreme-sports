01ec6044fa1ac3e0ee02181d66710d1cee273a3d
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-04-02 19:47:19 +0300

    Added EventInfoService

--- src/main/java/SimiAlex/com/gitlab/extremesportsapp/services/EventInfoService.java
+++ src/main/java/SimiAlex/com/gitlab/extremesportsapp/services/EventInfoService.java
@@ -5,6 +5,8 @@ import SimiAlex.com.gitlab.extremesportsapp.repositories.EventInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EventInfoService {
 
@@ -16,5 +18,25 @@ public class EventInfoService {
     public EventInfo createEventInfo(EventInfo eventInfo)
     {
         return eventInfoRepository.save(eventInfo);
+    }
+
+    public EventInfo getEventInfoById(Long id)
+    {
+        return eventInfoRepository.findById(id).orElseThrow();
+    }
+
+    public List<EventInfo> getEvents()
+    {
+        return (List<EventInfo>) eventInfoRepository.findAll();
+    }
+
+    public EventInfo updateEventInfo(EventInfo eventInfo)
+    {
+        return eventInfoRepository.save(eventInfo);
+    }
+
+    public void deleteEventInfo(Long id)
+    {
+        eventInfoRepository.deleteById(id);
     }
 }
