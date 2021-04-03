c4fb992948eaa6ee6046006d40856b1dbca42dea
smartywebdev0119 <smartwebdev0119@gmail.com>
2021-04-03 15:41:03 +0300

    Added RegionRequestModel

---
+++ src/main/java/SimiAlex/com/gitlab/extremesportsapp/model/request/RegionRequestModel.java
@@ -0,0 +1,34 @@
+package SimiAlex.com.gitlab.extremesportsapp.model.request;
+
+public class RegionRequestModel {
+
+    //fields
+    private Long id;
+    private Long countryId;
+    private String regionName;
+
+    //methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Long countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+}
